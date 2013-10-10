e557181bfee494b7442dbee1c148aee314bdac6f
Kodjo Baah <kodjo.baah@valtech.co.uk>
2013-10-10 01:03:26 +0100

    neo4j timeline

--- app/BuildInfrastructureData.scala
+++ app/BuildInfrastructureData.scala
@@ -2,46 +2,40 @@ import org.anormcypher.Cypher
 import com.whatamidoing.cypher.CypherInfrastructure
 
 object BuildInfrastructureData extends App {
-  
-  
-  
+
   val res19 = Cypher(CypherInfrastructure.createTimeLine()).execute()
   println("should have created timeline")
-  val months: List[String] = List("January","February","March","April","May","June","July","August","September","October","November","December")
-  val years: List[Int] = List(2012,2013,2014,2015,2016)
+  val months: List[String] = List("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+  val years: List[Int] = List(2012, 2013, 2014, 2015, 2016)
 
- for(year <- 0 to 4 ) {
-	val yearNode = Cypher(CypherInfrastructure.createYear(years(year))).execute()
-	val yearTimeLine = Cypher(CypherInfrastructure.linkTimeLineWithYear(years(year))).execute()   
-  } 
+  for (year <- 0 to 4) {
+    val yearNode = Cypher(CypherInfrastructure.createYear(years(year))).execute()
+    val yearTimeLine = Cypher(CypherInfrastructure.linkTimeLineWithYear(years(year))).execute()
+  }
 
- for(month <-0 to 11) {
-        println("month:"+month)
-        val actualMonth = month + 1;
-     
-	 	var monthDescription = months(month)+" - "+years(0)  
-        val monthNode = Cypher(CypherInfrastructure.createMonth(month+1,monthDescription)).execute() 
-	    val yearMonth = Cypher(CypherInfrastructure.linkMonthWithYear(monthDescription,years(1))).execute()
+  for (year <- 0 to 4) {
+    for (month <- 0 to 11) {
+      println("month:" + month)
+      val actualMonth = month + 1;
+
+      var monthDescription = months(month) + " - " + years(year)
+      val monthNode = Cypher(CypherInfrastructure.createMonth(month + 1, monthDescription)).execute()
+      val yearMonth = Cypher(CypherInfrastructure.linkMonthWithYear(monthDescription, years(1))).execute()
       //Associating a days to a month
-      for(day <- 0 to 30) {
-          println("day:"+day)
-          val actualDay = day + 1;
-	      var dayDescription =  "day "+actualDay+" - month "+actualMonth + "- year"+1
-	      val dayNode = Cypher(CypherInfrastructure.createDay(actualDay,dayDescription)).execute()
-		  val monthToDay = Cypher(CypherInfrastructure.linkMonthToDay(monthDescription,dayDescription)).execute()
-	   }
-	 
- }   
-  
- 
- 
-  
+      for (day <- 0 to 30) {
+        println("day:" + day)
+        val actualDay = day + 1;
+        var dayDescription = "day " + actualDay + " - month " + actualMonth + "- year" + 1
+        val dayNode = Cypher(CypherInfrastructure.createDay(actualDay, dayDescription)).execute()
+        val monthToDay = Cypher(CypherInfrastructure.linkMonthToDay(monthDescription, dayDescription)).execute()
+      }
+
+    }
+  }
+
   val res17 = Cypher(CypherInfrastructure.linkTimeLineWithYear(2014)).execute()
   val res18 = Cypher(CypherInfrastructure.linkTimeLineWithYear(2015)).execute()
-  
-  
-  
+
   println("done")
-  
-  
+
 }
