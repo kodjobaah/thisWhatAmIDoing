5803a2cb4422f0b7e04db03d2e0aa8d8acaa2293
Kodjo Baah <kodjo.baah@valtech.co.uk>
2013-10-10 01:17:00 +0100

    neo4j timeline

--- app/BuildInfrastructureData.scala
+++ app/BuildInfrastructureData.scala
@@ -13,96 +13,27 @@ object BuildInfrastructureData extends App {
     val yearTimeLine = Cypher(CypherInfrastructure.linkTimeLineWithYear(years(year))).execute()
   }
 
+  for(year <- 0 to 4) {
     for (month <- 0 to 11) {
       println("month:" + month)
-      val actualMonth = month + 1;
-
-      var monthDescription = months(month) + " - " + years(0)
+      val actualMonth = month + 1
+      val actualYear = year + 1
+      var monthDescription = months(month) + " - " + years(year)
       val monthNode = Cypher(CypherInfrastructure.createMonth(month + 1, monthDescription)).execute()
-      val yearMonth = Cypher(CypherInfrastructure.linkMonthWithYear(monthDescription, years(1))).execute()
+      val yearMonth = Cypher(CypherInfrastructure.linkMonthWithYear(monthDescription, years(year))).execute()
       //Associating a days to a month
       for (day <- 0 to 30) {
         println("day:" + day)
         val actualDay = day + 1;
-        var dayDescription = "day " + actualDay + " - month " + actualMonth + "- year" + 1
+        var dayDescription = "day " + actualDay + " - month " + actualMonth + "- year " + actualYear
         val dayNode = Cypher(CypherInfrastructure.createDay(actualDay, dayDescription)).execute()
         val monthToDay = Cypher(CypherInfrastructure.linkMonthToDay(monthDescription, dayDescription)).execute()
       }
 
     }
+}
 
-     for (month <- 0 to 11) {
-      println("month:" + month)
-      val actualMonth = month + 1;
 
-      var monthDescription = months(month) + " - " + years(1)
-      val monthNode = Cypher(CypherInfrastructure.createMonth(month + 1, monthDescription)).execute()
-      val yearMonth = Cypher(CypherInfrastructure.linkMonthWithYear(monthDescription, years(1))).execute()
-      //Associating a days to a month
-      for (day <- 0 to 30) {
-        println("day:" + day)
-        val actualDay = day + 1;
-        var dayDescription = "day " + actualDay + " - month " + actualMonth + "- year" + 1
-        val dayNode = Cypher(CypherInfrastructure.createDay(actualDay, dayDescription)).execute()
-        val monthToDay = Cypher(CypherInfrastructure.linkMonthToDay(monthDescription, dayDescription)).execute()
-      }
-
-    }
-
-      for (month <- 0 to 11) {
-      println("month:" + month)
-      val actualMonth = month + 1;
-
-      var monthDescription = months(month) + " - " + years(2)
-      val monthNode = Cypher(CypherInfrastructure.createMonth(month + 1, monthDescription)).execute()
-      val yearMonth = Cypher(CypherInfrastructure.linkMonthWithYear(monthDescription, years(1))).execute()
-      //Associating a days to a month
-      for (day <- 0 to 30) {
-        println("day:" + day)
-        val actualDay = day + 1;
-        var dayDescription = "day " + actualDay + " - month " + actualMonth + "- year" + 1
-        val dayNode = Cypher(CypherInfrastructure.createDay(actualDay, dayDescription)).execute()
-        val monthToDay = Cypher(CypherInfrastructure.linkMonthToDay(monthDescription, dayDescription)).execute()
-      }
-
-    }
-
-       for (month <- 0 to 11) {
-      println("month:" + month)
-      val actualMonth = month + 1;
-
-      var monthDescription = months(month) + " - " + years(3)
-      val monthNode = Cypher(CypherInfrastructure.createMonth(month + 1, monthDescription)).execute()
-      val yearMonth = Cypher(CypherInfrastructure.linkMonthWithYear(monthDescription, years(1))).execute()
-      //Associating a days to a month
-      for (day <- 0 to 30) {
-        println("day:" + day)
-        val actualDay = day + 1;
-        var dayDescription = "day " + actualDay + " - month " + actualMonth + "- year" + 1
-        val dayNode = Cypher(CypherInfrastructure.createDay(actualDay, dayDescription)).execute()
-        val monthToDay = Cypher(CypherInfrastructure.linkMonthToDay(monthDescription, dayDescription)).execute()
-      }
-
-    }
-
-        for (month <- 0 to 11) {
-      println("month:" + month)
-      val actualMonth = month + 1;
-
-      var monthDescription = months(month) + " - " + years(4)
-      val monthNode = Cypher(CypherInfrastructure.createMonth(month + 1, monthDescription)).execute()
-      val yearMonth = Cypher(CypherInfrastructure.linkMonthWithYear(monthDescription, years(1))).execute()
-      //Associating a days to a month
-      for (day <- 0 to 30) {
-        println("day:" + day)
-        val actualDay = day + 1;
-        var dayDescription = "day " + actualDay + " - month " + actualMonth + "- year" + 1
-        val dayNode = Cypher(CypherInfrastructure.createDay(actualDay, dayDescription)).execute()
-        val monthToDay = Cypher(CypherInfrastructure.linkMonthToDay(monthDescription, dayDescription)).execute()
-      }
-
-    }
-  
   println("done")
 
 }
