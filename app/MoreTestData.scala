02a122ff361f8ea4c26cc77f6027aeafad3ccc75
kodjo baah <kodjo_baah@hotmail.com>
2013-11-02 17:13:23 +0000

    Added view activity screen for token

---
+++ app/MoreTestData.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kodjobaah
+ * Date: 01/11/2013
+ * Time: 12:20
+ * To change this template use File | Settings | File Templates.
+ */
+object MoreTestData {
+
+}
---
+++ app/TestData.scala
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kodjobaah
+ * Date: 31/10/2013
+ * Time: 10:23
+ * To change this template use File | Settings | File Templates.
+ */
+class TestData {
+
+}
